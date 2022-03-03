a4f40f36ac103bc95345b92aafe488b8ecb451b2
Satya1819 <85295949+Satya1819@users.noreply.github.com>
2022-03-03 15:41:22 +0530

    import java.util.Scanner; class Division {     public static void main(String args[])     {          int N,M;          Scanner s=new Scanner(System.in);          N=s.nextInt();          M=s.nextInt();          System.out.println(N%M);     } }

---
+++ Modulus.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Division
+{
+    public static void main(String args[])
+    {
+         int N,M;
+         Scanner s=new Scanner(System.in);
+         N=s.nextInt();
+         M=s.nextInt();
+         System.out.println(N%M);
+    }
+}
